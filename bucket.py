7106d0d5579973fda1bddb50034c64eed8eda597
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-09-25 02:53:12 +0300

    values

--- bucket.py
+++ bucket.py
@@ -1,2 +1,6 @@
 import random # to generate random numbers
 import matplotlib.pyplot as plt # to sketch the graph
+
+# Changeable test numbers
+n = 750 # number of coins to throw
+N = 500 # number of empty buckets
