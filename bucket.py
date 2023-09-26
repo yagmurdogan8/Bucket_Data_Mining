1791d30271c82cc18dd9b3ef2466f622a75ed03f
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-09-26 14:03:57 +0300

    26.09

--- bucket.py
+++ bucket.py
@@ -4,3 +4,14 @@ import matplotlib.pyplot as plt # to sketch the graph
 # Changeable test numbers
 n = 750 # number of coins to throw
 N = 500 # number of empty buckets
+
+# defining a function to store distinct number's count for the random n and N values
+def bucket(n, N):
+    distinct_num_counts = [] # distinct number count
+    for i in range(n):
+        distinct_set = set()
+        for j in range(n):
+            rand_int = random.randint(1, N)
+            distinct_set.add(rand_int)
+        distinct_num_counts.append(len(distinct_set))
+    return distinct_num_counts
